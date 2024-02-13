f4edce97a8b19ca341809de356a989717b8dfe3c
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-02-13 11:33:42 +0100

    Clean up delay loader implementation

--- win32/delay-load.c
+++ win32/delay-load.c
@@ -1,6 +1,9 @@
-#ifndef WIN32_LEAN_AND_MEAN
+// Delay loader implementation for Windows. This is used to support loading
+// native addons from binaries that don't declare themselves as "bare.exe".
+//
+// See https://learn.microsoft.com/en-us/cpp/build/reference/understanding-the-helper-function
+
 #define WIN32_LEAN_AND_MEAN
-#endif
 
 #include <windows.h> // Must come first
 
@@ -8,12 +11,21 @@
 #include <string.h>
 
 static FARPROC WINAPI
-bare_delay_load (unsigned int event, DelayLoadInfo *info) {
-  if (event != dliNotePreLoadLibrary) return NULL;
+bare_delay_load (unsigned event, PDelayLoadInfo info) {
+  switch (event) {
+  case dliNotePreLoadLibrary:
+    if (_stricmp(info->szDll, "bare.exe") == 0) {
+      return (FARPROC) GetModuleHandle(NULL);
+    }
+    break;
 
-  if (_stricmp(info->szDll, "bare.exe") != 0) return NULL;
+  default:
+    return NULL;
+  }
 
-  return (FARPROC) GetModuleHandle(NULL);
+  return NULL;
 }
 
 const PfnDliHook __pfnDliNotifyHook2 = bare_delay_load;
+
+const PfnDliHook __pfnDliFailureHook2 = bare_delay_load;
